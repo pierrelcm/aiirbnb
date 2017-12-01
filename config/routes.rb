dfad31ff5abcd0ac1d5865e6963d85b911f7bfc0
Pierre <pierrelecam1@gmail.com>
2017-12-01 15:33:55 +0000

    Page modification de profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :path=>'',
-                    :path_names=>{:sign_in=>'login', :sign_out=>'logout', :edit=>'profile'}
+                    :path_names=>{:sign_in=>'login', :sign_out=>'logout', :edit=>'profile'},
+                    :controllers=>{:registrations=>'registrations'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  resources :users, only: [:show]
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
